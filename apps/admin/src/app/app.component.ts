3d9c6993580173eae2637e0fc41fc84277cc280d
sabelogumede <iamsabelogumede@gmail.com>
2021-07-06 14:43:34 +0200

    added admin app starter files onto our workspace. We now have two apps in our gummedia workspace- ngShop and admin

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'gummedia-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    title = 'admin';
+}
