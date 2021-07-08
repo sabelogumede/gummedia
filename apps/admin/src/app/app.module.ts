8c82cea9d936f736783f0a0adbba730868839322
sabelogumede <iamsabelogumede@gmail.com>
2021-07-08 18:18:03 +0200

    in shell components added sidebar component and routes in admin for shell and its children

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidearComponent } from './shared/sidear/sidear.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
+  },
+
+]
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent, ShellComponent, SidearComponent],
-  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
+  imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })],
   providers: [],
   bootstrap: [AppComponent]
 })
